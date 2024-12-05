8f56d6a3d17260f142471027b01af9dc82824873
nybia <adalel@my.bcit.ca>
2024-12-04 23:52:08 -0800

    Move unit tests to a seperate folder

--- tests/test_generate_term3.py
+++ tests/test_generate_term3.py
@@ -3,34 +3,36 @@ from game import generate_term3
 
 
 class Test(TestCase):
-    def test_generate_term3_solvable(self):
-        """Test generating third term for solvable problem."""
+    def test_generate_term3_solvable_negative(self):
+        """Test generating negative third term for solvable problem."""
         term1_value = 5.0
         term2_value = 10.0
         expected_value = -15.0
-        expected_string = "- 15.00"
+        expected_string = "- 15.0"
         
         actual_value, actual_string = generate_term3(term1_value, term2_value)
         
         self.assertEqual(expected_value, actual_value)
         self.assertEqual(expected_string, actual_string)
 
-    def test_generate_term3_unsolvable(self):
-        """Test generating third term for unsolvable problem."""
-        term1_value = 5.0
-        term2_value = 10.0
-        
+    def test_generate_term3_solvable_positive(self):
+        """Test generating positive third term for solvable problem."""
+        term1_value = -5.0
+        term2_value = -10.0
+        expected_value = 15.0
+        expected_string = "+ 15.0"
+
         actual_value, actual_string = generate_term3(term1_value, term2_value)
-        
-        self.assertEqual(float('inf'), actual_value)
-        self.assertEqual("+ 69^420", actual_string)
+
+        self.assertEqual(expected_value, actual_value)
+        self.assertEqual(expected_string, actual_string)
 
     def test_generate_term3_zero_sum(self):
         """Test generating third term that results in zero-sum."""
         term1_value = 5.0
         term2_value = -5.0
         expected_value = 0.0
-        expected_string = "+ 0.00"
+        expected_string = "+ 0.0"
         
         actual_value, actual_string = generate_term3(term1_value, term2_value)
         
