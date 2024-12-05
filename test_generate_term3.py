052a9bd0639e3219cfd19a1efb59442a3c035cdf
nybia <adalel@my.bcit.ca>
2024-12-04 21:54:36 -0800

    Fix Pep8 complaints

--- test_generate_term3.py
+++ test_generate_term3.py
@@ -28,7 +28,7 @@ class Test(TestCase):
         self.assertEqual("+ 69^420", actual_string)
 
     def test_generate_term3_zero_sum(self):
-        """Test generating third term that results in zero sum."""
+        """Test generating third term that results in zero-sum."""
         term1_value = 5.0
         term2_value = -5.0
         has_solution = True
@@ -38,4 +38,4 @@ class Test(TestCase):
         actual_value, actual_string = generate_term3(term1_value, term2_value, has_solution)
         
         self.assertEqual(expected_value, actual_value)
-        self.assertEqual(expected_string, actual_string) 
+        self.assertEqual(expected_string, actual_string)
